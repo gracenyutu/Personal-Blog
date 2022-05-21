d190e54b14bf1615b9c9589e7c210aa12609d0db
Grace Nyutu <graceynyutu@gmail.com>
2022-05-21 09:34:01 +0300

    update get_quotes function

--- blog/request.py
+++ blog/request.py
@@ -1,16 +1,19 @@
-# from app import app
+import requests,json
 
-import requests
+def get_quotes():
+    response = requests.get('https://famous-quotes4.p.rapidapi.com/random')
+    if response.status_code == 200:
+        quote = response.json()
+        return quote
 
-url = "https://famous-quotes4.p.rapidapi.com/random"
 
-querystring = {"category":"all","count":"2"}
+# querystring = {"category":"all","count":"2"}
 
-headers = {
-	"X-RapidAPI-Host": "famous-quotes4.p.rapidapi.com",
-	"X-RapidAPI-Key": "47e5fb643cmsh4c25729c915f11fp1f6156jsn60e461baee47"
-}
+# headers = {
+# 	"X-RapidAPI-Host": "famous-quotes4.p.rapidapi.com",
+# 	"X-RapidAPI-Key": "47e5fb643cmsh4c25729c915f11fp1f6156jsn60e461baee47"
+# }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+# response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+# print(response.text)
